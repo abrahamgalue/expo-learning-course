644c01e11f6552ec7f0c63001ef35d40cda422f7
Abraham Galue <abrahamgalue512@gmail.com>
2024-09-06 09:32:49 -0400

    Configured status bar, home screen and application icon.

--- App.js
+++ App.js
@@ -114,7 +114,7 @@ export default function App() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </GestureHandlerRootView>
   );
 }
